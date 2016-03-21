dd07a1b0f8fcbb38aa12aad825ea391e691301d0
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2016-03-21 15:58:53 -0400

    Add freeze support

--- speechtools/command_line/sct.py
+++ speechtools/command_line/sct.py
@@ -1,10 +1,11 @@
 import os
 import sys
+import multiprocessing
 
 from speechtools.gui.main import MainWindow, QtWidgets
 
 if __name__ == '__main__':
-
+    multiprocessing.freeze_support()
     app = QtWidgets.QApplication(sys.argv)
     main = MainWindow(app)
 
