59d4e14d633fb8194f832ee684e989f7834f580d
michaelasocolof <michaelasocolof@wpa073229.wireless.mcgill.ca>
2016-06-30 16:34:20 -0400

    Fix merge issues

--- tests/test_gui_widgets_export.py
+++ tests/test_gui_widgets_export.py
@@ -6,10 +6,7 @@ import collections
 
 def test_export_basiccolumnbox(acoustic_config, qtbot):
     w = ExportProfileDialog(acoustic_config, 'phone', None)
-<<<<<<< HEAD
-=======
     w.show()
->>>>>>> MontrealCorpusTools/master
     qtbot.addWidget(w)
     widget = w.BasicColumnBox.grid.itemAt(2).widget()
     qtbot.addWidget(widget)
@@ -23,8 +20,4 @@ def test_export_basiccolumnbox(acoustic_config, qtbot):
     widget2.setChecked(False)
     print (w.columnWidget.columns())
     assert w.columnWidget.columns()[0].attribute == ('phone','end')
-<<<<<<< HEAD
     assert len(w.columnWidget.columns()) == 1
-=======
-    assert len(w.columnWidget.columns()) == 1
->>>>>>> MontrealCorpusTools/master
