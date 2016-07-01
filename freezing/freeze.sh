3343a91af9fcd0f6277ced5eddcb31e3af04e002
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2016-07-01 14:41:31 -0400

    Update freeze.sh

--- freezing/freeze.sh
+++ freezing/freeze.sh
@@ -11,7 +11,7 @@ if [ `uname` == Darwin ]; then
         --exclude-module sphinx \
         --exclude-module docutils \
         --exclude-module scikits.samplerate \
-        --include-module setuptools
+        --hidden-import setuptools
         speechtools/command_line/sct.py
 
 fi
