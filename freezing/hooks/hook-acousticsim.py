89ff950af17330fc978143e7295eb8cfb976b631
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2016-07-01 11:53:39 -0400

    Update hook-acousticsim.py

--- freezing/hooks/hook-acousticsim.py
+++ freezing/hooks/hook-acousticsim.py
@@ -3,11 +3,13 @@ import os
 import sys
 
 from PyInstaller.utils.hooks import (
-    collect_data_files, collect_dynamic_libs)
+    collect_data_files, collect_dynamic_libs, copy_metadata)
 
-hiddenimports = ['cython', 'Cython', 'resampy']
+hiddenimports = ['cython', 'Cython', 'resampy', 'mock']
 
 datas = collect_data_files('resampy')
+
+datas += copy_metadata('mock')
 
 if sys.platform == 'darwin':
     binaries = [(os.path.expanduser('~/dev/tools/bin/reaper'), '.')]
