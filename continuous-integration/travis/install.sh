d7185de75fe2f2c250cb1c7805be4155c30dc225
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2016-08-15 09:47:42 -0400

    Update travis build

--- continuous-integration/travis/install.sh
+++ continuous-integration/travis/install.sh
@@ -12,9 +12,7 @@ if [ ! -d "$HOME/miniconda/miniconda/envs/test-environment" ]; then
   conda create -q -n test-environment python=$TRAVIS_PYTHON_VERSION setuptools atlas numpy sqlalchemy pytest scipy scikit-learn networkx
   source activate test-environment
   which python
-  conda install -c mmcauliffe pyqt5
-  pip install resampy
-  pip install -q coveralls coverage py2neo textgrid acousticsim pytest-qt
+  pip install -q pyqt5 resampy coveralls coverage py2neo textgrid acousticsim pytest-qt
   pip install -q https://github.com/MontrealCorpusTools/PolyglotDB/archive/master.zip
   pip install -q https://github.com/vispy/vispy/archive/master.zip
 else
@@ -25,9 +23,9 @@ fi
 
 if [ ! -d "$HOME/neo4j/neo4j" ]; then
   mkdir -p $HOME/neo4j
-  wget http://dist.neo4j.org/neo4j-community-3.0.3-unix.tar.gz
-  tar -xzf neo4j-community-3.0.3-unix.tar.gz -C $HOME/neo4j
-  mv $HOME/neo4j/neo4j-community-3.0.3 $HOME/neo4j/neo4j
+  wget http://dist.neo4j.org/neo4j-community-3.0.4-unix.tar.gz
+  tar -xzf neo4j-community-3.0.4-unix.tar.gz -C $HOME/neo4j
+  mv $HOME/neo4j/neo4j-community-3.0.4 $HOME/neo4j/neo4j
   sed -i.bak s/#dbms.security.auth_enabled=false/dbms.security.auth_enabled=false/g $HOME/neo4j/neo4j/conf/neo4j.conf
   sed -i.bak s/dbms.directories.import=import/#dbms.directories.import=import/g $HOME/neo4j/neo4j/conf/neo4j.conf
 else
